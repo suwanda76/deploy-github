9b3e3a7032ed868c3d5a5c72612f5baecea5998c
suwanda76 <suwanda.chen@gmail.com>
2024-08-19 13:46:24 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>First Project to Deploy on GitHub</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
